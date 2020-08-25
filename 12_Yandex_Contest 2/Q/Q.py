66e23d0fceae22a646b004b4a4d34db2abfeba67
Korsakov Pavel <pavelkpv@gmail.com>
2020-08-26 03:06:17 +0800

    25/08/20 Tast Q test 7 pass

--- 12_Yandex_Contest 2/Q/Q.py
+++ 12_Yandex_Contest 2/Q/Q.py
@@ -1,21 +1,27 @@
 """
-P. Списочная очередь
+Q. Дек
 
-Любимый вариант очереди Тимофея - очередь, написанная с использованием
-связного списка. Помогите ему с реализацией. Очередь должна поддерживать
-методы get, put, size.
+Гоша решил реализовать структуру данных Дек, максимальный размер которого
+определяется заданным числом. Методы push_back, push_front, pop_front, pop_back
+работали корректно. Но, если в деке было много элементов, программа работала
+очень долго. Дело в том, что не все операции выполнялись за O(1). Помогите
+Гоше! Напишите эффективную реализацию.
 
 Формат ввода
 В первой строке записано количество команд n - целое число, не превосходящее
-1000. В каждой из следующих n строк записана команда: get, put, или size.
+5000. Во второй строке записано число m - максимальный размер дека. Он не
+превосходит 1000. В следующих n строках записана одна из команд:
+push_back value
+push_front value
+pop_front
+pop_back
+value - целое число, по модулю не превосходящее 1000.
 
 Формат вывода
-При вызове метода get напечатайте возвращаемое значение. Если метод get
-вызывается у пустой очереди, нужно напечатать 'error'. При вызове метода
-size - вывести размер очереди.
-
-Примечания
-Все операции должны выполняться за O(1).
+При вызове команд pop_front и pop_back нужно вывести возвращаемое значение.
+Если они вызываются для пустого дека — напечатайте 'error'. Если команда
+push_back или push_front вызывается для дека, размер которого равен максимально
+возможному, тоже нужно вывести 'error'.
 """
 
 
@@ -30,7 +36,16 @@ class MyQueueSized:
     def isEmpty(self):
         return self.size_queue == 0
 
-    def get(self):
+    def pop_back(self):
+        if self.isEmpty():
+            return 'error'
+        x = self.queue[self.tail]
+        self.queue[self.tail] = None
+        self.tail = (self.tail - 1) % self.max_n
+        self.size_queue -= 1
+        return x
+
+    def pop_front(self):
         if self.isEmpty():
             return 'error'
         x = self.queue[self.head]
@@ -39,10 +54,23 @@ class MyQueueSized:
         self.size_queue -= 1
         return x
 
-    def put(self, item):
+    def push_front(self, item):
         if self.size_queue < self.max_n:
-            self.queue[self.tail] = item
-            self.tail = (self.tail + 1) % self.max_n
+            if self.queue[self.head] is None:
+                self.queue[self.head] = item
+            else:
+                self.head = (self.head - 1) % self.max_n
+                self.queue[self.head] = item
+            self.size_queue += 1
+            return ''
+
+    def push_back(self, item):
+        if self.size_queue < self.max_n:
+            if self.queue[self.tail] is None:
+                self.queue[self.tail] = item
+            else:
+                self.tail = (self.tail - 1) % self.max_n
+                self.queue[self.tail] = item
             self.size_queue += 1
             return ''
 
@@ -59,20 +87,23 @@ string, template = input_file[0], input_file[1]
 output_file = ''
 
 count_commands = int(input_file[0])
-commands = input_file[1:]
-queue = MyQueueSized(count_commands)
+queue_size = int(input_file[1])
+commands = input_file[2:]
+queue = MyQueueSized(queue_size)
 output_file = ''
 
 for i in commands:
     command = i.split()
-    if command[0] == 'put':
-        output_file += str(queue.put(int(command[1])))
-    elif command[0] == 'get':
-        output_file += str(queue.get()) + '\n'
-    elif command[0] == 'size':
-        output_file += str(queue.size()) + '\n'
+    if command[0] == 'push_back':
+        output_file += str(queue.push_back(int(command[1])))
+    elif command[0] == 'push_front':
+        output_file += str(queue.push_front(int(command[1])))
+    elif command[0] == 'pop_front':
+        output_file += str(queue.pop_front()) + '\n'
+    elif command[0] == 'pop_back':
+        output_file += str(queue.pop_back()) + '\n'
 
-# print(output_file)
+print(output_file)
 f = open('output.txt', 'w')
 f.write(output_file + '\n')
 f.close()
