3489334a91bb866a79f340e4e45fb5dd3c6c42ef
Korsakov Pavel <pavelkpv@gmail.com>
2020-10-05 02:26:57 +0800

    05/10/20 Sprint 15 Final A

--- 15_Yandex_Final_Tasks/A/A_v2.py
+++ 15_Yandex_Final_Tasks/A/A_v2.py
@@ -32,19 +32,38 @@ A. Кондратиева пирамида
 Примечания
 Нужно реализовать алгоритм пирамидальной сортировки.
 """
-# 34980813
+# 35144179
 import heapq
 
 
 class participant_of_race:
-    def __init__(self, index_number, riders_name_point_str):
-        temp = riders_name_point_str.split()
-        self.amount_positive_point = sum(
-            i for i in list(map(int, temp[1:])) if i > 0)
-        self.name = temp[0]
+    def __init__(self,
+                 index_number,
+                 amount_positive_point,
+                 name,
+                 text_value,
+                 detect_kondratiy):
+        self.amount_positive_point = amount_positive_point
+        self.name = name
         self.index_number = index_number
-        self.text_value = riders_name_point_str
-        self.detect_kondratiy = set('kondratiy') <= set(self.name)
+        self.text_value = text_value
+        self.detect_kondratiy = detect_kondratiy
+
+    @classmethod
+    def from_string(cls, index_number, riders_name_point_str):
+        temp = riders_name_point_str.split()
+        amount_positive_point = sum(
+            i for i in list(map(int, temp[1:])) if i > 0)
+        name = temp[0]
+        index_number = index_number
+        text_value = riders_name_point_str
+        detect_kondratiy = set('kondratiy') <= set(name)
+        data = cls(index_number,
+                   amount_positive_point,
+                   name,
+                   text_value,
+                   detect_kondratiy)
+        return data
 
     def __str__(self):
         return self.text_value
@@ -59,6 +78,7 @@ class participant_of_race:
                 if self.index_number > other.index_number:
                     return True
 
+
 def sorting_riders(riders_list):
     riders1 = list()
     riders2 = list()
@@ -71,6 +91,7 @@ def sorting_riders(riders_list):
     for _ in range(len(riders2)):
         riders1.append(heapq.heappop(riders2))
     return riders1
+
 
 def test():
     with open('input1.txt') as f:
@@ -97,11 +118,12 @@ def test():
         input_file) == 'name 5 0\nname 2 3\nname 5 -4\nname 4 1\nname 5\n', 'input4.txt error\n' + str(
         main(input_file))
 
+
 def main(input_str):
     input_str = input_str.rstrip().split('\n')
     riders_list = list()
     for i, item in enumerate(input_str[1:]):
-        riders_list.append(participant_of_race(i, item))
+        riders_list.append(participant_of_race.from_string(i, item))
     answer_str = ''
     for item in sorting_riders(riders_list):
         answer_str += str(item) + '\n'
